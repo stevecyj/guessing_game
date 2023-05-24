b9950024e10c752b48df047c6dfa6613e93696e1
stevecyj <ycsteven.tsao@gmail.com>
2023-05-24 22:55:42 +0800

    feat: update console output to support Chinese language input
    - Translate console output to Chinese
    - Prompt user for input and read it from stdin
    - Print user's input as part of output
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("請猜測一個數字：");
+    println!("請輸入你的猜測數字。");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("讀取行數失敗");
+
+    println!("你猜測的數字是：{guess}");
 }
