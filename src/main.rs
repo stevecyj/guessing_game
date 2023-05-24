f2132f3a5a0306350d0bb062d1b14688bece4e90
stevecyj <ycsteven.tsao@gmail.com>
2023-05-25 00:13:14 +0800

    feat: refactor number guessing game with random secret number generation
    - Add usage of the `rand` crate for generating a secret number
    - Print the secret number to the console
    - Remove a line of code prompting the user to guess before they input a number
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("請猜測一個數字：");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("祕密數字是：{}", secret_number);
+
     println!("請輸入你的猜測數字。");
 
     let mut guess = String::new();
